3c9770c02a0ee24895b76601d42e9a5dbe17cc2c
Tristan Damron <tristandamron@protonmail.com>
2022-09-09 22:33:08 -0700

    Install Python dependencies with install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 python3 -m venv $(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")lib/python
 ln -s $(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")bin/pkgenv /usr/local/bin/pkgenv
+$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")lib/python/bin/python3 -m pip install -r requirements.txt
